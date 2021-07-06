de115f55ce759f8d92076676d03ba0c152c3797c
Margot Payebin <margotpayebin@MacBook-Pro-de-Margot.local>
2021-07-06 16:08:16 +0200

    e3

---
+++ ex_03/index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+if (isset($_GET["name"])){
+    echo "Hello ". $_GET["name"];
+    $_SESSION["name"] = $_GET["name"];
+}
+else if (isset($_SESSION["name"])) {
+    echo "Hello ". $_SESSION["name"];
+}
+else {
+    echo "Hello platypus";
+}
+?>
